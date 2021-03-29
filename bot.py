453a51fced10df2e31f4731961734f3db37e5d5b
Tanrrr <tanrrrbronson@gmail.com>
2021-03-29 04:27:11 -0600

    Started The Bot!
    added wake up and prefix command

---
+++ bot.py
@@ -0,0 +1,79 @@
+from datetime import date, datetime
+import os
+import datetime 
+import discord
+import json
+from discord.ext import commands
+
+def get_prefix(bot, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix= get_prefix)
+#Remove default HELP command
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    #Wake up message
+    cur_time = datetime.datetime.now().strftime("%#I:%M %p")
+    guild_ammount = len(bot.guilds)
+
+    if datetime.datetime.now().hour < 10:
+        print(f"Good Morning! It is currently {cur_time}, and I'm currently connected to {guild_ammount} servers!")
+    elif datetime.datetime.now().hour < 17:
+        print(f"Good Afternoon! It is currently {cur_time}, and I'm currently connected to {guild_ammount} servers!")
+    elif datetime.datetime.now().hour < 24:
+        print(f"Good Evening! It is currently {cur_time}, and I'm currently connected to {guild_ammount} server!")  
+
+    #Bot status
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.watching, name="out for Ruka!"))
+
+@bot.event
+async def on_guild_join(guild):
+    #Adds prefix on guild join
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = '>'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.event
+async def on_guild_remove(guild):
+    #Removes prefix on guild leave
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+
+#Simple ping command
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')        
+
+#Prefix setting command
+@bot.command()
+async def prefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+
+
+#Reads token and runs bot
+with open('token.txt') as f:
+    token = f.read()
+
+bot.run(token)
