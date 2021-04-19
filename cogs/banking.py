b54d87fa38c5cf4f9c946ed4fef549245f64bbb0
Tanrrr <tanrrrbronson@gmail.com>
2021-04-19 03:45:52 -0600

    Update banking.py

--- cogs/banking.py
+++ cogs/banking.py
@@ -1,3 +1,4 @@
+from discord.ext.commands import cooldowns
 from database.profiledb import ProfileDB
 
 import discord
@@ -5,32 +6,38 @@ from discord.ext import commands
 
 db = ProfileDB()
 
+
+
 class Banking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.Cog.listener()
+    async def on_member_join(self,ctx, member):
+        user = member
+
+        db.open_account(ctx.guild.id, user.id)
+
+    @commands.command(aliases=['b', 'bal'])
     async def balance(self, ctx):
         user = ctx.author
 
-        db.open_account(ctx.author.id)
+        db.open_account(ctx.guild.id, ctx.author.id)
 
-        bal = db.get_bank_data(ctx.author.id)
+        bal = db.get_bank_data(ctx.guild.id, ctx.author.id)
 
-        embed=discord.Embed(description=f'{bal}')
-        embed.set_author(name=f'{user} Balance', icon_url=f'{user.avatar_url}')
+        embed=discord.Embed(title=f' {bal} $')
+        embed.set_author(name=f"{user}'s Balance", icon_url=f'{user.avatar_url}')
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def add(self, ctx, deltamoney):
-        db.open_account(ctx.author.id)
+    async def add(self, ctx, deltamoney: int):
 
-        delmoney = int(deltamoney)
+        db.open_account(ctx.guild.id, ctx.author.id)
 
-        db.change_money(ctx.author.id, delmoney)
+        db.change_money(ctx.guild.id, ctx.author.id, deltamoney)
 
-        await ctx.send("Pog")
-
+        await ctx.send(f"Added {deltamoney}$ to account!")
 
 def setup(bot):
     bot.add_cog(Banking(bot))
